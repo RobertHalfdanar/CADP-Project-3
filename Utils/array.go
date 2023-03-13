4e1b5a67862d401dc3915b7f751724d36d86c8ed
Róbert Ingi Hálfdanarson <66384324+RobertHalfdanar@users.noreply.github.com>
2023-03-13 20:26:01 +0000

    feat: :sparkles: Added find and remove function for UDPAddr arrays

--- Utils/array.go
+++ Utils/array.go
@@ -1,5 +1,7 @@
 package Utils
 
+import "net"
+
 // WrapIndex receives and index and a length and returns the index if it is in the range of the length
 // otherwise wraps the index around the array
 func WrapIndex(index int, length int) int {
@@ -18,4 +20,20 @@ func Contains(e []int32, item int32) bool {
 		}
 	}
 	return false
+}
+
+func Find(s []*net.UDPAddr, e *net.UDPAddr) int {
+	for i, a := range s {
+		if a == e {
+			return i
+		}
+	}
+	return -1
+}
+
+func Remove(s []*net.UDPAddr, e *net.UDPAddr) []*net.UDPAddr {
+	index := Find(s, e)
+
+	s[index] = s[len(s)-1]
+	return s[:len(s)-1]
 }
