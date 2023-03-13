d7cda228fb146b10c048caa5774b1770ede4c695
Róbert Ingi Hálfdanarson <66384324+RobertHalfdanar@users.noreply.github.com>
2023-03-13 18:11:36 +0000

    feat(Client): :sparkles: Client can send messages to the server

--- Client/client.go
+++ Client/client.go
@@ -1,20 +1,97 @@
-package client
+package Client
 
 import (
-	"CADP-Project-3/raft"
+	"CADP-Project-3/Raft"
+	"CADP-Project-3/Utils"
+	"bufio"
 	"fmt"
-	proto "google.golang.org/protobuf/proto"
-	"log"
+	"net"
 	"os"
+	"strings"
 )
 
-func main() {
-	server := os.Args[1]
-	fmt.Println("Input server argument: ", server)
-	// Read from stdin and send
-	data, err := proto.Marshal(&Raft.Raft{Message: &Raft.Raft_CommandName{CommandName: "command"}})
+// Server is a struct that represents a server
+type Server struct {
+	Addr *net.UDPAddr
+	IP   string
+	Port int32
+}
+
+// Send sends a message to the server, the server is specified in the Server struct
+// if the server is not available, it will print an error message.
+func (node *Server) Send(msg *Raft.Raft) {
+	conn, err := net.DialUDP("udp", nil, node.Addr)
+	defer conn.Close()
+
 	if err != nil {
-		log.Fatal(err)
+		fmt.Println("\u001B[31mFailed to connect to host\u001B[0m")
+		return
 	}
-	fmt.Println("Example marshalled command: ", data)
+
+	// Send the message to the server
+	err = Utils.WriteToConn(conn, msg)
+
+	if err != nil {
+		fmt.Println("\u001B[31mFailed to send to host\u001B[0m")
+		return
+	}
+}
+
+// The server that we are connected to
+var server = Server{}
+
+// exitCommandHandler handles the exit command
+func exitCommandHandler() {
+	fmt.Println("exit command")
+}
+
+// sendCommandHandler handles all other commands and sends them to the server
+func sendCommandHandler(cmd string) {
+	message := &Raft.Raft{Message: &Raft.Raft_CommandName{CommandName: cmd}}
+
+	server.Send(message)
+
+	fmt.Println("Command sent: " + cmd)
+}
+
+// commandsHandler find the correct handler for a given command
+func commandsHandler(cmd string) {
+
+	cmd = strings.TrimSpace(cmd)
+
+	if Utils.HasPunctuation(cmd) {
+		fmt.Println("\u001B[31mCommand cannot contain punctuation\u001B[0m")
+		return
+	}
+
+	switch cmd {
+	case "exit":
+		exitCommandHandler()
+	default:
+		sendCommandHandler(cmd)
+	}
+}
+
+// commandsListener listens for commands from the user, when a command is received it will be process it.
+func commandsListener() {
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		fmt.Print("Command> ")
+		cmd, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println(err)
+			break
+		}
+
+		commandsHandler(cmd)
+	}
+}
+
+// Start initializes the server connection for the client and starts the command listener
+func Start(serverAddress string) {
+
+	server.IP, server.Port = Utils.ToIPAndPort(serverAddress)
+	server.Addr = Utils.CreateUDPAddr(serverAddress)
+
+	commandsListener()
 }
--- raftclient.go
+++ raftclient.go
@@ -1,4 +1,18 @@
 package main
 
+import (
+	"CADP-Project-3/Client"
+	"fmt"
+	"os"
+)
+
 func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("\u001B[31mMissing server address\u001B[0m")
+		fmt.Println("Usage: go run raftclient.go server-host:server-port")
+	}
+
+	serverAddress := os.Args[1]
+
+	Client.Start(serverAddress)
 }
