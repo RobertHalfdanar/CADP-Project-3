64b456011030ba1065acbbfa6b439f88445ded87
Róbert Ingi Hálfdanarson <66384324+RobertHalfdanar@users.noreply.github.com>
2023-03-13 13:47:54 +0000

    refactor: :hammer: Did some refactoring

--- Client/client.go
+++ Client/client.go
@@ -1,1 +1,20 @@
-package Client
+package client
+
+import (
+	"CADP-Project-3/raft"
+	"fmt"
+	proto "google.golang.org/protobuf/proto"
+	"log"
+	"os"
+)
+
+func main() {
+	server := os.Args[1]
+	fmt.Println("Input server argument: ", server)
+	// Read from stdin and send
+	data, err := proto.Marshal(&Raft.Raft{Message: &Raft.Raft_CommandName{CommandName: "command"}})
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Example marshalled command: ", data)
+}
--- raftclient.go
+++ raftclient.go
@@ -1,20 +1,4 @@
 package main
 
-import (
-	"CADP-Project-3/raft"
-	"fmt"
-	proto "google.golang.org/protobuf/proto"
-	"log"
-	"os"
-)
-
 func main() {
-	server := os.Args[1]
-	fmt.Println("Input server argument: ", server)
-	// Read from stdin and send
-	data, err := proto.Marshal(&Raft.Raft{Message: &Raft.Raft_CommandName{CommandName: "command"}})
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("Example marshalled command: ", data)
 }
